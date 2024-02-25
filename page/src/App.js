289b757f17c1d3dbfb93457cbee705ed0ac66aec
Sameer <114623580+SamSwnshi@users.noreply.github.com>
2024-02-25 23:58:09 +0530

    Final

--- page/src/App.js
+++ page/src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const [page, setPage] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postPerpage] = useState(10);
+  const [totalPages, setTotalPages] = useState(1);
+
+  const lastIdx = currentPage * postPerpage;
+  const firstIdx = lastIdx - postPerpage;
+  const currentPost = page.slice(firstIdx, lastIdx);
+
+  // Fetch data from API
+  useEffect(() => {
+    fetchData(currentPage);
+  }, [currentPage]);
+
+  const fetchData = (pageNumber) => {
+    axios
+      .get(
+        " https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json"
+      )
+      .then((res) => {
+        setPage(res.data);
+        setTotalPages(10);
+      })
+      .catch((error) => console.error("Error", error));
+  };
+  const handlePrevious = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
+  const handleNext = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Employee Data table</h1>
+      <table className="table-head">
+        <thead className="row-head">
+          <tr >
+            <th > ID</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Role</th>
+          </tr>
+        </thead>
+        <tbody>
+          {currentPost.map((items) => (
+            <tr key={items.id} >
+              <td>{items.id}</td>
+              <td>{items.name}</td>
+              <td>{items.email}</td>
+              <td>{items.role}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <div  className="pagination">
+        <button onClick={handlePrevious} className="btns">
+          Previous
+        </button>
+        <span className="number">{currentPage}</span>
+        <button onClick={handleNext} className="btns">
+          Next
+        </button>
+      </div>
     </div>
   );
 }
